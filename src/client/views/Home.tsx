3f97b66fd11f93e84ba25271a05bd9e4052b6815
Kaley925 <Kaley925@gmail.com>
2021-11-22 16:41:50 -0600

    Made the routing paths for the various pages

--- src/client/views/Home.tsx
+++ src/client/views/Home.tsx
@@ -1,1 +1,11 @@
+import * as React from 'react';
+
+const Home = () => {
+    return(
+        <>
+        <h1 className="justify-content-center mt-5 row">Hello from the Home page</h1>
+        </>
+    )
+}
+
 export default Home ;
