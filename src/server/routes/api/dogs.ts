df6c8ede585bed3086020e2a2836900eec25ca36
unknown <richdanmac@gmail.com>
2021-12-01 14:42:59 -0600

    debugging compiling errors

--- src/server/routes/api/dogs.ts
+++ src/server/routes/api/dogs.ts
@@ -7,7 +7,7 @@ const router = Router();
 
 router.get('/', passport.authenticate('jwt'), async (req: ReqUser, res) => {
     try {
-        const apiKey = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJoWEkxQlQyOHcwRDY0c2ZkdW0wNlhpc1NNZHJYcDF2bVBoWGlTdnhhVXZqbW10SGJvWCIsImp0aSI6IjA3NmYwODRmZDQ5NThiNDRlOTgwMGU5ZTk2YTg0ZjNjZmYyZTE2YjliZjZhYTY1ZjBiNTljNTJjMjI4MjA3ZWJhZjA3MDZjY2YwZjUyYjY2IiwiaWF0IjoxNjM4MzA1ODkxLCJuYmYiOjE2MzgzMDU4OTEsImV4cCI6MTYzODMwOTQ5MSwic3ViIjoiIiwic2NvcGVzIjpbXX0.sgA4gIG3OL2rllfDBzy65BlGvnFuSgELUC3wTdbznDn5TSNVHRLlDP5kB52kfeWaTIr7xPiwu1PuJUbxMwVQd3nQddDjveZ3FE4wjf_qbxtaO0r2pMb6RUBuIWKgL3CHSvJLNyJVEdujKQABs-5uBvbytAeJpk0U-axNWl6qrE84ZvDC0nUM5KV0s_ubyY-MP_aV1GfH5sf4ueDyYFnoEidnStYki3JZt1vNwU6zdn2O1W91x0bjdyq-6NFU5R5wZ7_ofgGmC9oM2kjWGBVUrYH8SG6Rgdt4DyyDpdKrZbj6eaWHn4vHxOwBUE1MdsIDuhlltkkR8AdyFTpnrmbN2Q';
+        const apiKey = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJoWEkxQlQyOHcwRDY0c2ZkdW0wNlhpc1NNZHJYcDF2bVBoWGlTdnhhVXZqbW10SGJvWCIsImp0aSI6IjdlZjliYzgxZTAyMDkzMGMzNDg2OThhMGJiM2QzMzM4NmNkMzg4NzBmN2VmMDM3M2E4YmNlYTFiZDQ5Nzk1YTM0ZWU2Y2RhN2EyZTBjMWM1IiwiaWF0IjoxNjM4Mzg3MTc0LCJuYmYiOjE2MzgzODcxNzQsImV4cCI6MTYzODM5MDc3NCwic3ViIjoiIiwic2NvcGVzIjpbXX0.qDuhVSXn5PFhx6haweCZ0K2tn7Dxs2woEKwqKpvA39tMlN2Mm3kCGnKI7A0WmAoynCODJjy8ra4EunotWWXKQDPftSOBFi6aurbskbGaTO5ok64woltEP-BFsHIlgCuU7fi78fGKPl0xALSRh9nZLsP7Xivw2yv9x8WcePIC_022ozLknDcfz9ih_OEAXuLAWHNjromon6leXlVI1MmuJV_TPbLrgyEfi93kE4g7MBf96tyKO2MZXkjxw0-0lRLJSMbWL_WybxyGQEp780TBF1N7qAuo4QUX-Uku9m00bTe8msPRLNZHSA01WzksVKYZuj_yCcurgclvzady4cW6XQ";
         const resp = await fetch(`https://api.petfinder.com/v2/animals/`, {
             headers: {
                 'Authorization': `Bearer ${apiKey}`
@@ -33,7 +33,7 @@ router.get('/', passport.authenticate('jwt'), async (req: ReqUser, res) => {
             }
         });
 
-        res.json(allDogs.filter(animal => animal));
+        res.json(allDogs.filter((animal: any) => animal));
 
     } catch (error) {
         res.status(500).json({ error: 'Something went wrong!' });
