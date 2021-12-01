64b5c9e1b1f1db26d94ced0945586c959b7b5349
Kaley925 <Kaley925@gmail.com>
2021-12-01 09:34:49 -0600

    josh made me do it

---
+++ src/client/App.tsx
@@ -0,0 +1,27 @@
+
+import * as React from "react";
+import { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./views/Home";
+import Cats from "./views/Cats";
+import Dogs from "./views/Dogs";
+import Profile from "./views/Profile";
+
+
+/* HOOK REACT EXAMPLE */
+
+const App = () => {
+  console.log();
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Cats" element={<Cats />} />
+        <Route path="/Dogs" element={<Dogs />} />
+        <Route path="/Profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
---
+++ src/client/index.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import { render } from "react-dom";
+import App from "./App";
+import "./scss/app";
+import "./scss/home";
+import "./scss/signuplogin.scss";
+
+
+render(<App />, document.getElementById("root"));
