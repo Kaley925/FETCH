4e93ae4b94c616fd888ab50ae97fe0b81f233728
LoopyConundrm <sungsam2444@gmail.com>
2021-12-06 12:31:36 -0600

    Finished styling profile page and sidebar

---
+++ src/client/components/Sidebar.tsx
@@ -0,0 +1,58 @@
+import React, { useState, useEffect, useRef } from "react";
+import * as IoIcons from "react-icons/Io";
+import * as AiIcons from "react-icons/ai";
+import { Link } from "react-router-dom";
+import "./../../scss/sidebar";
+import { IconContext } from "react-icons";
+
+function Sidebar() {
+  const [sidebar, setSidebar] = useState(false);
+  const showSidebar = () => setSidebar(!sidebar);
+
+  return (
+    <>
+      <IconContext.Provider value={{ color: "#fead12" }}>
+        <div className="sidebar">
+          <Link to="#" className="menu-paw">
+            <IoIcons.IoMdPaw onClick={showSidebar} />
+          </Link>
+        </div>
+        <div className={sidebar ? "menu active" : "menu"}>
+          <ul className="menu-items">
+            <li className="menu-toggle">
+              <Link to="#" className="menu-bars">
+                <AiIcons.AiOutlineClose onClick={showSidebar} />
+              </Link>
+            </li>
+            <li className="sidebar-text">
+              <Link to="#">
+                <AiIcons.AiFillAccountBook />
+                <span className="text-span" >Change Avatar</span>
+              </Link>
+            </li>
+            <li className="sidebar-text">
+              <Link to="#">
+                <AiIcons.AiFillAccountBook />
+                <span className="text-span">Change Name</span>
+              </Link>
+            </li>
+            <li className="sidebar-text">
+              <Link to="#">
+                <AiIcons.AiFillAccountBook />
+                <span className="text-span">Change Email</span>
+              </Link>
+            </li>
+            <li className="sidebar-text">
+              <Link to="#">
+                <AiIcons.AiFillAccountBook />
+                <span className="text-span">Change Password</span>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </IconContext.Provider>
+    </>
+  );
+}
+
+export default Sidebar;
--- src/client/views/Profile.tsx
+++ src/client/views/Profile.tsx
@@ -1,11 +1,8 @@
 import React, { useState, useEffect, useRef } from "react";
 import Navigation from "../components/Navigation";
-import Sidebar from "../components/Sidebar/Sidebar";
+import Sidebar from "../components/Sidebar";
 import "../scss/profile";
-import { Link } from "react-router-dom";
-import Signup from "../components/Signup";
 import { gsap } from "gsap";
-
 
 const Profile = () => {
   const headerRef = useRef();
@@ -14,59 +11,45 @@ const Profile = () => {
     gsap.to(headerRef.current, { opacity: 1, delay: 1 });
   }, []);
 
-  // Modal signup
-  const [showSignup, setShowSignup] = useState(false);
-  const handleSignupClose = () => setShowSignup(false);
-  const handleSignupShow = () => setShowSignup(true);
-
   return (
     <>
-      <main ref={headerRef} id="main">
-      <Navigation bgView="#FBF4EA" handleLogin={undefined} handleSignup={undefined}/>
-      <Sidebar />
-      
-      <div className="container d-flex justify-content-center mt-5">
-        <div className="card prof p-3 py-4">
-          <div className="text-center">
-            {" "}
-            <img
-              src="https://i.imgur.com/stD0Q19.jpg"
-              width="100"
-              className="rounded-circle"
-            />
-            <hr />
-            <h3 className=" name mt-2">Maria Samantha</h3>{" "}
-            <span className="mt-1 clearfix">Starbucks barista</span>
-            <div className="row mt-3 mb-3">
-              <div className="col-md-6">
-                <h5>Pets Owned</h5> <span className="num">3</span>
-              </div>
-              <div className="col-md-6">
-                <h5>Age</h5> <span className="num">25</span>
-              </div>
-            </div>
-            <p className="mt-4">
-              I am a barista at Starbucks and studying psychology at The University of Alabama
-            </p>
-            <div className="profile mt-5">
+      <main ref={headerRef}>
+        <Navigation
+          bgView="#FBF4EA"
+          handleLogin={undefined}
+          handleSignup={undefined}
+        />
+
+        <div className="container d-flex justify-content-center mt-5">
+          <Sidebar />
+          <div className="card prof p-3 py-4">
+            <div className="text-center">
               {" "}
+              <img
+                src="https://i.imgur.com/stD0Q19.jpg"
+                width="100"
+                className="rounded-circle"
+              />
+              <hr />
+              <h3 className=" name mt-2">Maria Samantha</h3>{" "}
+              <span className="mt-1 clearfix">Starbucks barista</span>
+              <div className="row mt-3 mb-3">
+                <div className="col-md-6">
+                  <h5>Pets Owned</h5> <span className="num">3</span>
+                </div>
+                <div className="col-md-6">
+                  <h5>Age</h5> <span className="num">25</span>
+                </div>
+              </div>
+              <p className="mt-4">
+                I'm a barista at Starbucks and studying psychology at The
+                University of Alabama🎓
+              </p>
+              <div className="profile mt-5"> </div>
             </div>
           </div>
         </div>
-      </div>
       </main>
-      {/* <div className="container change">
-        <div>
-          <ul>
-            <p>Account Settings</p>
-            <p>Account Settings</p>
-            <p>Account Settings</p>
-            <p>Account Settings</p>
-            <p>Account Settings</p>
-            <p>Account Settings</p>
-          </ul>
-        </div>
-      </div> */}
     </>
   );
 };
